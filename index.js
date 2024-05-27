c5412f037ea5bbe7e04599ac717a220900d6a214
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-05-27 20:28:18 +0300

    json-server-auth

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server-auth"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001

0839977edc6b168b917201df23bb797109c0006c
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-05-27 19:05:56 +0300

    new commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const guards_1 = require("./guards");
+const users_1 = require("./users");
+// @ts-ignore shut the compiler up about defining in two steps
+const middlewares = [users_1.default, guards_1.default];
+Object.defineProperty(middlewares, "rewriter", {
+  value: guards_1.rewriter,
+  enumerable: false,
+});
+module.exports = middlewares;
 
 server.use(middlewares);
 server.use(router);
